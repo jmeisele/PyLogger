fac6d2675941bb1a05fb311bcb371da6f05f7199
Jason Eisele <jmeisele@yahoo.com>
2019-10-04 07:55:00 -0500

    Logging example

---
+++ scratch_logger.py
@@ -0,0 +1,25 @@
+import logging 
+"""
+In the logging module there are 6 levels of 'logs'
+0~NOTSET
+10~DEBUG
+20~INFO
+30~WARNING
+40~ERROR
+50~CRITICAL
+Loggers will only write messages with level greater 
+than or equal to our set level
+"""
+# Create and configure logger
+FORMAT = '%(asctime)s %(levelname)s - %(message)s'
+logging.basicConfig(filename = 'C:/users/eiselja/desktop/lumberjack.log', level = logging.DEBUG, format=FORMAT, filemode='w')
+logger = logging.getLogger()
+
+# Test the logger
+logger.debug('This is a debug statement')
+logger.info('Our first message.')
+logger.warning('This is a warning')
+logger.error('This is an error')
+logger.critical('This is a critical message')
+
+# print(logger.level)
