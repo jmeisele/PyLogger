3bb7a6626ba7e5775e2b95b2ea7922609a817de0
JASON EISELE <jason.eisele@daimler.com>
2019-10-16 13:31:13 -0500

    refactored for PEP8 code style

--- scratch_logger.py
+++ scratch_logger.py
@@ -1,4 +1,4 @@
-import logging 
+import logging
 """
 In the logging module there are 6 levels of 'logs'
 0~NOTSET
@@ -7,19 +7,18 @@ In the logging module there are 6 levels of 'logs'
 30~WARNING
 40~ERROR
 50~CRITICAL
-Loggers will only write messages with level greater 
+Loggers will only write messages with level greater
 than or equal to our set level
 """
 # Create and configure logger
 FORMAT = '%(asctime)s %(levelname)s - %(message)s'
-logging.basicConfig(filename = 'C:/users/eiselja/desktop/lumberjack.log', level = logging.DEBUG, format=FORMAT, filemode='w')
+logging.basicConfig(filename='C:/users/eiselja/desktop/lumberjack.log',
+                    level=logging.DEBUG, format=FORMAT, filemode='w')
 logger = logging.getLogger()
-
 # Test the logger
 logger.debug('This is a debug statement')
 logger.info('Our first message.')
 logger.warning('This is a warning')
 logger.error('This is an error')
 logger.critical('This is a critical message')
-
 # print(logger.level)
